70fa7a1867daf7d3e493f2085ce71ec2f179e9e9
ItsthatMeatball <itsthatmeatball@gmail.com>
2025-06-01 21:30:04 -0400

    Update TTS-extension.js

--- TTS-extension.js
+++ TTS-extension.js
@@ -2,6 +2,10 @@
   'use strict';
 
   class VoiceRSSTTS {
+    constructor() {
+      this.isPlaying = false;
+    }
+
     getInfo() {
       return {
         id: 'voicerssTTS',
@@ -14,7 +18,17 @@
             arguments: {
               TEXT: { type: Scratch.ArgumentType.STRING, defaultValue: 'Hello world!' },
               LANG: { type: Scratch.ArgumentType.STRING, defaultValue: 'en-us' },
-              APIKEY: { type: Scratch.ArgumentType.STRING, defaultValue: 'd64251b07f5b4c44a36e4731ebd6642b' }
+              APIKEY: { type: Scratch.ArgumentType.STRING, defaultValue: 'YOUR_API_KEY' }
+            }
+          },
+          {
+            opcode: 'speakWait',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'say [TEXT] until done at language [LANG] with key [APIKEY]',
+            arguments: {
+              TEXT: { type: Scratch.ArgumentType.STRING, defaultValue: 'Hello world!' },
+              LANG: { type: Scratch.ArgumentType.STRING, defaultValue: 'en-us' },
+              APIKEY: { type: Scratch.ArgumentType.STRING, defaultValue: 'YOUR_API_KEY' }
             }
           }
         ]
@@ -29,6 +43,25 @@
 
       const audio = new Audio(url);
       audio.play();
+    }
+
+    async speakWait(args) {
+      const text = encodeURIComponent(args.TEXT);
+      const lang = args.LANG;
+      const key = args.APIKEY;
+      const url = `https://api.voicerss.org/?key=${key}&hl=${lang}&src=${text}`;
+
+      const audio = new Audio(url);
+      this.isPlaying = true;
+
+      // Return a Promise to wait until audio ends
+      await new Promise(resolve => {
+        audio.onended = () => {
+          this.isPlaying = false;
+          resolve();
+        };
+        audio.play();
+      });
     }
   }
 
